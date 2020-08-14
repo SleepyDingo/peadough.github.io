d006b259b78e47dd2da8b2403c853f0259a85972
SleepyDingo <69660768+SleepyDingo@users.noreply.github.com>
2020-08-14 15:21:29 +1000

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,8 @@
 var logins = [["HeadOfCoding", "12345"], ["SleepyDingo", "12345"]]
 
 function checkLogin() {
-  
+  a
 } 
+const loginForm = document.getElementById("psw);
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
